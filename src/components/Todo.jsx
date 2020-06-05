e618cead07df7b4f116fbf727b5cbdd19486e63f
Matthew Choat <matthewchoat@gmail.com>
2020-06-04 23:53:17 -0500

    finished assignment. Added animated list items, Reactstrap styling, and Local Storage

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,35 +1,61 @@
 import React, { useState, useEffect } from 'react';
-    import './Todo.css';
+import './Todo.css';
+import {Container, Form, Input, Button, Collapse, Card, Fade} from 'reactstrap';
 
-    function Task({ task, index, completeTask, removeTask }) {
+    function Task({ task, index, completeTask, uncompleteTask, removeTask }) {
+        const [fadeIn, setFadeIn] = useState(true);
+        const toggle = () => setFadeIn(!fadeIn);
+
         return (
+            <Fade in={fadeIn}>
             <div
                 className="task"
                 style={{ textDecoration: task.completed ? "line-through" : "" }}
             >
                 {task.title}
-                <button style={{ background: "red" }} onClick={() => removeTask(index)}>x</button>
-                <button onClick={() => completeTask(index)}>Complete</button>
+                <Button close style={{ background: "red", padding: "2px"}} onClick={() => removeTask(index)}></Button>
+                <Button color="success" style={{padding: "3px"}} onClick={() => { task.completed ? uncompleteTask(index) : completeTask(index)}}>Complete</Button>
             </div>
+            </Fade>
         );
     }
 
     function Todo() {
+        Storage.prototype.setObj = function(key, obj) {
+            return this.setItem(key, JSON.stringify(obj))
+        }
+        Storage.prototype.getObj = function(key) {
+            return JSON.parse(this.getItem(key))
+        }
+
         const [tasksRemaining, setTasksRemaining] = useState(0);
-        const [tasks, setTasks] = useState([
+        const [tasks, setTasks] = useState(
+            localStorage.getObj('myTasksInLocalStorage') || [
             {
-                title: "Grab some Pizza",
+                title: "Morning Yoga",
                 completed: true
             },
             {
-                title: "Do your workout",
+                title: "Cold shower",
                 completed: true
             },
             {
-                title: "Hangout with friends",
+                title: "100 Pushups",
                 completed: false
             }
         ]);
+
+        useEffect(() => {
+            localStorage.setObj('myTasksInLocalStorage', tasks);
+        }, [tasks]);
+
+        const onChange = event => setTasks(event.target.tasks);
+
+        const [isOpen, setIsOpen] = useState(true);
+
+        const toggle = () => setIsOpen(!isOpen);
+
+
 
         useEffect(() => { 
           setTasksRemaining(tasks.filter(task => !task.completed).length) 
@@ -50,6 +76,12 @@ import React, { useState, useEffect } from 'react';
             setTasks(newTasks);
         };
 
+        const uncompleteTask = index => {
+            const newTasks = [...tasks];
+            newTasks[index].completed = false;
+            setTasks(newTasks);
+        };
+
         const removeTask = index => {
             const newTasks = [...tasks];
             newTasks.splice(index, 1);
@@ -57,49 +89,56 @@ import React, { useState, useEffect } from 'react';
         };
 
         return (
-            <div className="todo-container">
-                <div className="header">Tasks remaining: ({tasksRemaining}). You got this!</div>
-                <div className="tasks">
-                    {tasks.map((task, index) => (
-                        <Task
-                        task={task}
-                        index={index}
-                        completeTask={completeTask}
-                        removeTask={removeTask}
-                        key={index}
-                        />
-                    ))}
-                </div>
+
+            <Container className="todo-container">
+                <h1 className="header">Tasks remaining: ({tasksRemaining}). You got this!</h1>
+                
+                <Container fluid className="tasks">
+                    <Collapse isOpen={isOpen}>
+                            <Card body style={{ backgroundColor: 'rgba(247, 247, 247, 0)'}}>
+                                    {tasks.map((task, index) => (
+                                        <Task
+                                        task={task}
+                                        index={index}
+                                        completeTask={completeTask}
+                                        uncompleteTask={uncompleteTask}
+                                        removeTask={removeTask}
+                                        key={index}
+                                        onChange={onChange}
+                                        />
+                                        ))}
+                            </Card>
+                    </Collapse>
+                </Container>
+                <Button className="hide" color="secondary" size="sm" onClick={toggle} style={{ margin: '.5rem' }}>Hide</Button>
+                <Button className="hide" color="danger" size="sm"  onChange={onChange} onClick={() => (setTasks([]))}>Reset</Button>
                 <div className="create-task" >
-                    <CreateTask addTask={addTask} />
+                    <CreateTask  onChange={onChange} addTask={addTask} />
                 </div>
-            </div>
+            </Container>
         );
     }
 
-
-
     function CreateTask({ addTask }) {
         const [value, setValue] = useState("");
-
         const handleSubmit = e => {
             e.preventDefault();
             if (!value) return;
 
             addTask(value);
             setValue('');
-
         }
-
         return (
-            <form onSubmit={handleSubmit}><input
+            <Form onSubmit={handleSubmit}><Input
                 type="text"
                 className="input"
+                name="input"
+                id="input"
                 value={value}
-                placeholder="Add a new task"
+                placeholder="Type a new task and press [Enter]"
                 onChange={e => setValue(e.target.value)}
                   />
-            </form>
+            </Form>
         );
     }
 
